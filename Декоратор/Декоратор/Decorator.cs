58d1a47e66c52f749464d8baff9129fd682709d4
Sasha <117905137+Ghostinchik@users.noreply.github.com>
2023-04-06 12:48:52 +0300

    Decorator

---
+++ Декоратор/Декоратор/Decorator.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Декоратор
+{
+    internal class Decorator
+    {
+        public abstract class CardComponentBase
+        {
+            public abstract int Width { get; }
+            public abstract int Height { get; }
+
+            public abstract void Draw(Graphics graphics);
+        }
+
+        public class CardComponent : CardComponentBase
+        {
+            public override int Width { get; }
+            public override int Height { get; }
+
+            public CardComponent(int width, int height)
+            {
+                Width = width;
+                Height = height;
+            }
+
+            public override void Draw(Graphics graphics)
+            {
+                // Draw a basic card with white background
+                graphics.FillRectangle(Brushes.White, 0, 0, Width, Height);
+                graphics.DrawRectangle(Pens.Black, 0, 0, Width - 1, Height - 1);
+            }
+        }
+
+        public abstract class CardDecorator : CardComponentBase
+        {
+            protected CardComponentBase _card;
+
+            public CardDecorator(CardComponentBase card)
+            {
+                _card = card;
+            }
+        }
+
+        public class BorderDecorator : CardDecorator
+        {
+            private Color _borderColor;
+            private int _borderWidth;
+
+            public BorderDecorator(CardComponentBase card, Color borderColor, int borderWidth) : base(card)
+            {
+                _borderColor = borderColor;
+                _borderWidth = borderWidth;
+            }
+
+            public override int Width
+            {
+                get { return _card.Width + _borderWidth * 2; }
+            }
+
+            public override int Height
+            {
+                get { return _card.Height + _borderWidth * 2; }
+            }
+
+            public override void Draw(Graphics graphics)
+            {
+                // Draw the border
+                graphics.FillRectangle(new SolidBrush(_borderColor), 0, 0, Width, Height);
+
+                // Draw the decorated card inside the border
+                _card.Draw(graphics);
+            }
+        }
+
+        public class ImageDecorator : CardDecorator
+        {
+            private Image _image;
+
+            public ImageDecorator(CardComponentBase card, Image image) : base(card)
+            {
+                _image = image;
+            }
+
+            public override int Width
+            {
+                get { return _card.Width; }
+            }
+
+            public override int Height
+            {
+                get { return _card.Height; }
+            }
+
+            public override void Draw(Graphics graphics)
+            {
+                // Draw the decorated card first
+                _card.Draw(graphics);
+
+                // Draw the image on top of the card
+                graphics.DrawImage(_image, 0, 0, Width, Height);
+            }
+        }
+    }
+}
---
+++ Декоратор/Декоратор/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Декоратор
+{
+    public partial class Form1 : Form
+    {
+        private Decorator.CardComponentBase _card;
+        public Form1()
+        {
+            InitializeComponent();
+            _card = new Decorator.CardComponent(400, 350); // create a basic card component
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Graphics graphics = pictureBox1.CreateGraphics();
+
+            if (checkBox1.Checked)
+            {
+                _card = new Decorator.BorderDecorator(_card, Color.Black, 10);
+            }
+
+            if (checkBox2.Checked)
+            {
+                Image image = Image.FromFile(" E:\\прог\\4 курс\\КПЗ\\Декоратор\\Dimiourgos_repeating_japanese_pattern_83a1f057-aded-44ca-a2bb-28a2109718d1.png ");
+                _card = new Decorator.ImageDecorator(_card, image);
+                _card = new Decorator.BorderDecorator(_card, Color.Red, 10);
+            }
+
+            if (checkBox3.Checked)
+            {
+                Image image = Image.FromFile(" E:\\прог\\4 курс\\КПЗ\\Декоратор\\Dimiourgos_repeating_japanese_pattern_f1c40323-9ae3-452b-b690-8b2f4949d7ab.png ");
+                _card = new Decorator.ImageDecorator(_card, image);
+                _card = new Decorator.BorderDecorator(_card, Color.Blue, 10);
+            }
+
+
+            _card.Draw(graphics);
+        }
+    }
+}
